a7a07be1c9e1a2238447e1781b83a5685928498a
opsonc <58789337+opsonc@users.noreply.github.com>
2023-06-11 12:54:22 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_hanoip-user
-add_lunch_combo omni_hanoip-userdebug
-add_lunch_combo omni_hanoip-eng
+add_lunch_combo twrp_hanoip-user
+add_lunch_combo twrp_hanoip-userdebug
+add_lunch_combo twrp_hanoip-eng
